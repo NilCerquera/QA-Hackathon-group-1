6d7b323d44e3735ca36659d3ab93b7ab12ae4679
NilCerquera <nifecerquera@gmail.com>
2024-09-06 16:22:12 -0500

    BugBank first

--- BugBank/Automated testing/QABugBank.py
+++ BugBank/Automated testing/QABugBank.py
@@ -5,6 +5,7 @@ import Data
 
 class QABugBank:
     create_account = (By.XPATH, ".//div[@id='__next']//button[text()='Registrar']")
+    input_email = (By.XPATH, "//input[@name='email']")
     input_name = (By.XPATH, "//input[@name='name']")
     def __init__(self, driver):
         self.driver = driver
@@ -13,5 +14,9 @@ class QABugBank:
         self.driver.find_element(*self.create_account).click()
 
     def set_email(self):
-        self.driver.find_element(*self.input_name).click()
+        self.driver.find_element(*self.input_email).click()
+        self.driver.find_element(*self.input_name).send_keys(Data.name)
+
+    def set_name(self):
+        self.driver.find_element(*self.input_email).click()
         self.driver.find_element(*self.input_name).send_keys(Data.name)
